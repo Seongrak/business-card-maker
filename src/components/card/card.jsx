02b33bd06d1e439fe31232f907933675ee36ef2e
rocky <rock9247@naver.com>
2022-02-01 13:38:28 -0500

    editor page layout

---
+++ src/components/card/card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./card.module.css";
+
+const DEFAULT_IMAGE = "/images/default_logo.png";
+const Card = (props) => {
+  const { name, company, title, email, message, theme, fileName, fileURL } =
+    props.card;
+  const url = fileURL || DEFAULT_IMAGE;
+
+  return (
+    <li className={`${styles.card} ${getTheme(theme)}`}>
+      <img src={url} alt="profile img" className={styles.profile} />
+      <div className={styles.info}>
+        <h1 className={styles.name}>{name}</h1>
+        <p className={styles.company}>{company}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.email}>{email}</p>
+        <p className={styles.message}>{message}</p>
+      </div>
+    </li>
+  );
+};
+
+function getTheme(theme) {
+  switch (theme) {
+    case "dark":
+      return styles.dark;
+    case "light":
+      return styles.light;
+    case "colorful":
+      return styles.colorful;
+    default:
+      throw new Error(`unknown theme: ${theme}`);
+  }
+}
+
+export default Card;
--- src/components/maker.jsx
+++ src/components/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Editor from "./editor";
 import Footer from "./footer";
@@ -7,6 +7,41 @@ import styles from "./maker.module.css";
 import Preview from "./preview";
 
 const Maker = (props) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Rocky-test",
+      company: "TD bank-test",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "rocky@gm.com",
+      message: "TEST REACT STUDY:)",
+      fileName: "rocky",
+      fileURL: "",
+    },
+    {
+      id: "2",
+      name: "Rocky-test2",
+      company: "TD bank-test",
+      theme: "light",
+      title: "Software Engineer",
+      email: "rocky@gm.com",
+      message: "TEST REACT STUDY:)",
+      fileName: "rocky",
+      fileURL: "",
+    },
+    {
+      id: "3",
+      name: "Rocky-test3",
+      company: "TD bank-test",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "rocky@gm.com",
+      message: "TEST REACT STUDY:)",
+      fileName: "rocky",
+      fileURL: "",
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     props.authService.logout();
@@ -23,8 +58,8 @@ const Maker = (props) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
